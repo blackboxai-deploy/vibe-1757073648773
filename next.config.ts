2f5a66642a08d3fefc60c93305e518d24244c8fb
blackboxai-deploy <deploy@blackboxai.dev>
2025-09-05 17:31:31 +0530

    Add next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,33 @@
+import type { NextConfig } from "next";
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+});
+
+const nextConfig: NextConfig = {
+  experimental: {
+    optimizePackageImports: ['@radix-ui/react-icons'],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        pathname: '/photos/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+        pathname: '/workspace-*/image/**',
+      },
+    ],
+  },
+};
+
+export default withPWA(nextConfig);
